e823fd53330ac20c9147b50373881158db157784
dennissammy77 <dennissammy77@gmail.com>
2025-05-22 05:34:31 +0300

    sql practices

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age,sex,color,temperament, alive) values ('Mr. Chocolate',10,'M','brown','not sure',1);
+INSERT INTO bears (name, age,sex,color,temperament, alive) values ('Rowdy',10,'M','brown','not sure',0);
+INSERT INTO bears (name, age,sex,color,temperament, alive) values ('Tabitha',10,'M','brown','not sure',1);
+INSERT INTO bears (name, age,sex,color,temperament, alive) values ('Sergeant Brown',10,'M','brown','not sure',1);
+INSERT INTO bears (name, age,sex,color,temperament, alive) values ('Melissa',10,'M','brown','not sure',0);
+INSERT INTO bears (name, age,sex,color,temperament, alive) values ('Melissa',10,'M','brown','not sure',1);
+INSERT INTO bears (name, age,sex,color,temperament, alive) values ('Wendy',10,'M','brown','not sure',0);
+INSERT INTO bears (name, age,sex,color,temperament, alive) values (NULL,10,'M','brown','not sure',1);
